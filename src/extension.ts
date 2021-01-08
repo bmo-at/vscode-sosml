36870277a29c51eafcbc3dbb2b43d96ef95dd3c5
bmo-at <jan-robin@aumann.org>
2021-01-09 00:44:04 +0100

    Refactor opening of interpreter

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,17 @@ import { SMLView } from "./webview";
 export function activate(context: vscode.ExtensionContext) {
 	console.log('vscode-sosml is now active');
 
-	let interpretCommand = vscode.commands.registerTextEditorCommand('vscode-sosml.interpret', (textEditor) => {
+	let interpretCommand = vscode.commands.registerTextEditorCommand('vscode-sosml.interpret', async (textEditor) => {
 		if (textEditor.document.languageId !== 'sml') {
 			vscode.window.showErrorMessage('Open File is not an SML file');
 			return;
 		}
-		SMLView.createOrShow(textEditor.document);
+		createOrUpdate(textEditor.document);
 	});
 
-	let onChangeSMLFile = vscode.workspace.onDidSaveTextDocument((document) => {
+	let onChangeSMLFile = vscode.workspace.onDidSaveTextDocument(async (document) => {
 		if (document.languageId !== 'sml') { return; }
-		if (SMLView.currentView?.isCurrent(document)) { SMLView.createOrShow(document); }
+		if (SMLView.currentView) { createOrUpdate(document); }
 	});
 
 	let onChangeConfig = vscode.workspace.onDidChangeConfiguration((configChangeEvent) => {
@@ -26,3 +26,16 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() { }
+
+async function createOrUpdate(document: vscode.TextDocument) {
+	if (!SMLView.currentView) { SMLView.create(document); } else {
+		if (SMLView.currentView.isCurrent(document)) {
+			SMLView.update(document);
+		} else {
+			vscode.window.showInformationMessage("A different file is currently being interpreted. Switch to interpreting this file?");
+			if (await vscode.window.showQuickPick(["Yes", "No"]) === "Yes") {
+				SMLView.create(document);
+			} else { return; }
+		}
+	}
+}
